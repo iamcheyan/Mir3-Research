eca13f0c222e9620101206eb2218db98ab5ff82f
cheyan <iamcheyan@gmail.com>
2026-09-15 14:10:06 +0900

    watchdog: register maishufang CMS goal

--- scripts/goal_watchdog.sh
+++ scripts/goal_watchdog.sh
@@ -112,6 +112,7 @@ GOALS=(
   # [archived 2026-08-16 E5完成] "01a00878-9625-7000-a1a0-4efc341cb9cb|/home/tetsuya/.omp/agent/sessions/-development-Mir3-Research/2026-08-16T02-48-35-621Z_01a00878-9625-7000-a1a0-4efc341cb9cb.jsonl|e5-data|/home/tetsuya/development/Mir3-Research|编辑器E5参数数据层化"
   # [archived 2026-08-16 E6完成] "01a0088b-76a3-7000-be68-9a857beba360|/home/tetsuya/.omp/agent/sessions/-development-Mir3-Research/2026-08-16T03-09-12-739Z_01a0088b-76a3-7000-be68-9a857beba360.jsonl|e6-fix|/home/tetsuya/development/Mir3-Research|编辑器E6审计问题修复"
   # [active 2026-08-28] "01a04700-67cc-7000-ae6e-6d9bc99df455|/home/tetsuya/.omp/agent/sessions/-rime/2026-08-28T06-13-24-044Z_01a04700-67cc-7000-ae6e-6d9bc99df455.jsonl|rime-ext|/home/tetsuya/rime|SBZR外部词频参考"
+  "01a0a377-fe90-7000-9c3c-078eaa805633|/home/tetsuya/.omp/agent/sessions/-development-maishufang/2026-09-15T05-09-05-296Z_01a0a377-fe90-7000-9c3c-078eaa805633.jsonl|maishufang-cms|/home/tetsuya/development/maishufang|麦书房GitHub CMS实现"
 )
 
 CHECK_ONLY=0
